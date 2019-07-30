33ef7e4010d4a04564b7ac9e0e593b161329f870
Nomosudro <purnima.nomosudro@thermofisher.com>
2019-07-30 10:30:33 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace standardLib
+{
+    public class Class1<Typeofinputs>
+    {
+        public Typeofinputs add(Typeofinputs a, Typeofinputs b)
+        {
+            dynamic n1 = a;
+            dynamic n2 = b;
+            return (n1 + n2);
+        }
+        public Typeofinputs sub(Typeofinputs a, Typeofinputs b)
+        {
+            dynamic n1 = a;
+            dynamic n2 = b;
+            return (n1 - n2);
+        }
+        public Typeofinputs multiply(Typeofinputs a, Typeofinputs b)
+        {
+            dynamic n1 = a;
+            dynamic n2 = b;
+            return (n1 * n2);
+        }
+        public Typeofinputs division(Typeofinputs a, Typeofinputs b)
+        {
+            dynamic n1 = a;
+            dynamic n2 = b;
+            return (n1 / n2);
+        }
+        public int sumofnums(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
